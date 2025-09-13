4d9d044932b935289fd244e95117dc2c4fbd1de8
coder-nitin07 <nitinthakur87504@gmail.com>
2025-09-13 16:08:10 +0530

    Create getWorkSpace API

--- backend/controllers/workSpaceController.js
+++ backend/controllers/workSpaceController.js
@@ -4,16 +4,16 @@ const prisma = require('../config/prisma');
 const createWorkSpace = async (req, res)=>{
     try {
         const { name, description } = req.body;
-        const userId = req.user?.userId;
-if (!userId) return res.status(401).json({ message: 'User not found in token' });
-
-        
-
         if(!name){
             return res.status(400).json({ 
                 success: false,
                 message: 'Please filled all the required fields' 
             });
+        }
+
+        const userId = req.user?.userId;
+        if (!userId){
+            return res.status(401).json({ message: 'User not found in token' });
         }
 
         // crete workspace and add owner as creator
@@ -48,4 +48,29 @@ if (!userId) return res.status(401).json({ message: 'User not found in token' })
     }
 };
 
-module.exports = { createWorkSpace };
+// Get WorkSpace Details
+const getWorkSpace = async (req, res)=>{
+    try {
+        const workSpaceId = req.params.id;
+        
+        const workSpace = await prisma.workspace.findUnique({
+            where: { id: workSpaceId },
+            include: {
+                owner: true,
+                members: { include: { user:true } },
+                boards: true
+            }
+        });
+
+        if(!workSpace){
+            return res.status(404).json({ message: 'WorkSpace not found' });
+        }
+
+        res.status(200).json({ message: 'Fetched the WorkSpace Successfully', workSpace });
+    } catch (err) {
+        console.log("Server Error", err);
+        res.status(500).json({ message: 'Something went wrong' });
+    }
+};
+
+module.exports = { createWorkSpace, getWorkSpace };
--- backend/routes/workSpaceRoutes.js
+++ backend/routes/workSpaceRoutes.js
@@ -1,8 +1,9 @@
 const express = require('express');
 const { authMiddleware } = require('../middlewares/authMiddleware');
-const { createWorkSpace } = require('../controllers/workSpaceController');
+const { createWorkSpace, getWorkSpace } = require('../controllers/workSpaceController');
 const workSpaceRouter = express.Router();
 
 workSpaceRouter.post('/create', authMiddleware, createWorkSpace);
+workSpaceRouter.get('/getWorkSpace/:id', authMiddleware, getWorkSpace);
 
 module.exports = { workSpaceRouter };
