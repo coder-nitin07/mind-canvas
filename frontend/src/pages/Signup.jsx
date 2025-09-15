fd6f5ed21b056a4bab9ff0b691953f04149fa7cc
coder-nitin07 <nitinthakur87504@gmail.com>
2025-09-15 15:02:38 +0530

    Create SignUp Page

--- frontend/src/pages/Signup.jsx
+++ frontend/src/pages/Signup.jsx
@@ -1,8 +1,66 @@
-import React from 'react'
+import React, { useState } from 'react'
 
 const Signup = () => {
+  const [ name, setName ] = useState('');
+  const [ email, setEmail ] = useState('');
+  const [ password, setPassword ] = useState('');
+
+  const handleSignup = async (e)=>{
+    e.preventDefault();
+
+    // console.log('Signup name', name, " email ", email, " password ", password);
+
+    try {
+      const response = await fetch('http://localhost:3000/auth/register', {
+        method: 'POST',
+        headers: {
+            'Content-Type': 'application/json'
+        },
+        body: JSON.stringify({ name, email, password })
+      })
+
+      if(response.ok){
+        const data = await response.json();
+
+        console.log('SignUp Successfully', data);
+      }
+      else{
+        const errorData = await response.json();
+        console.log('SignUp failed', errorData.message || 'Unknown Error');
+      }
+    } catch (err) {
+      console.log('Error while connecting to the Backend Server', err);
+    }
+  };
   return (
-    <div>Signup</div>
+    <>
+      <h2>Signup</h2>
+
+      <form onSubmit={ handleSignup }>
+        <input
+          type='name'
+          placeholder='Enter name'
+          value={ name }
+          onChange={(e)=> setName(e.target.value) }
+        />
+
+         <input
+          type='email'
+          placeholder='Enter email'
+          value={ email }
+          onChange={(e)=> setEmail(e.target.value) }
+        />
+
+         <input
+          type='password'
+          placeholder='Enter password'
+          value={ password }
+          onChange={(e)=> setPassword(e.target.value) }
+        />
+
+        <button type='submit'>Singup</button>
+      </form>
+    </>
   )
 }
 
