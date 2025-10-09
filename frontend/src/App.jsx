43d824157956def0d7be55d013cba8524b64f922
coder-nitin07 <nitinthakur87504@gmail.com>
2025-10-09 15:28:30 +0530

    Create ProtectedRoute

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -2,6 +2,8 @@ import { BrowserRouter as Router, Route, Routes } from 'react-router-dom'
 import './App.css'
 import Signup from './pages/Signup'
 import Login from './pages/Login'
+import ProtectedRoute from './utils/ProtectedRoute'
+import Dashboard from './pages/Dashboard'
 
 function App() {
 
@@ -10,6 +12,16 @@ function App() {
       <Routes>
           <Route path='/login' element={<Login />} />
           <Route path='/signup' element={<Signup />} />
+
+          {/* Protected Routes */}
+          <Route
+              path='/dashboard'
+              element={
+                <ProtectedRoute>
+                  <Dashboard />
+                </ProtectedRoute>
+              }
+          />
       </Routes>
     </Router>
   )
